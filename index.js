142c6fe4858e801234c562a65617752e9817e5ba
kelvyn <kudufo@gmail.com>
2023-10-11 10:29:07 +0100

    first stage of music book using API

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+const API_URL = "https://api.deezer.com/search?q=";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: [] });
+});
+
+app.post("/artists", async (req, res) => {
+  const artist = req.body.artistName;
+  try {
+    const response = await axios.get(API_URL + artist);
+    const artistData = response.data.data;
+    console.log(artistData);
+    const artistInfo = artistData.map((info) => {
+      return info;
+    });
+    console.log(artistInfo)
+    res.render("index.ejs", { content: artistInfo });
+   
+  } catch (error) {
+    console.error(error);
+  } //
+});
+("");
+
+app.listen(port, () => {
+  console.log(`Server has started in port ${port}`);
+});
